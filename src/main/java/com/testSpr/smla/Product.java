8c8a061934ac9fea3cf6a5ce1158e3597e62ed17
Shubham <shubham1932@gmail.com>
2018-07-21 13:36:22 +0530

    commit new class

---
+++ src/main/java/com/testSpr/smla/Product.java
@@ -0,0 +1,36 @@
+package com.testSpr.smla;
+
+public class Product {
+
+	private String productId;
+	private String productName;
+	private String productDesc;
+	private String productCategory;
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	public String getProductCategory() {
+		return productCategory;
+	}
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+	
+	
+	
+}
